fe7d15a7324fdccb8c7dbf1cf5289f34e166d179
Sportrediska <anatolyskorov@yandex.ru>
2025-09-30 23:20:07 +0300

    urls и data в отдельных модулях

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,63 +1,20 @@
 import pytest
 from selenium import webdriver
-
+from urls import Urls
 from pages.home_page import HomePage
+from data_for_tests import DataForTests as data
 
 
 class TestHomePage:
     driver = None
-    BASE_URL = 'https://qa-scooter.praktikum-services.ru/'
-    faq = [
-        {
-            'index': 0,
-            'question': 'Сколько это стоит? И как оплатить?',
-            'answer': 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.'
-        },
-        {
-            'index': 1,
-            'question': 'Хочу сразу несколько самокатов! Так можно?',
-            'answer': 'Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.'
-        },
-        {
-            'index': 2,
-            'question': 'Как рассчитывается время аренды?',
-            'answer': 'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.'
-        },
-        {
-            'index': 3,
-            'question': 'Можно ли заказать самокат прямо на сегодня?',
-            'answer': 'Только начиная с завтрашнего дня. Но скоро станем расторопнее.'
-        },
-        {
-            'index': 4,
-            'question': 'Можно ли продлить заказ или вернуть самокат раньше?',
-            'answer': 'Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.'
-        },
-        {
-            'index': 5,
-            'question': 'Вы привозите зарядку вместе с самокатом?',
-            'answer': 'Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.'
-        },
-        {
-            'index': 6,
-            'question': 'Можно ли отменить заказ?',
-            'answer': 'Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.'
-        },
-        {
-            'index': 7,
-            'question': 'Я жизу за МКАДом, привезёте?',
-            'answer': 'Да, обязательно. Всем самокатов! И Москве, и Московской области.'
-        }
-
-    ]
 
     @classmethod
     def setup_class(cls):
         cls.driver = webdriver.Firefox()
 
-    @pytest.mark.parametrize('faq', faq)
+    @pytest.mark.parametrize('faq', data.faq)
     def test_faq_question_corresponds_answer(self, faq):
-        self.driver.get(self.BASE_URL)
+        self.driver.get(Urls.BASE_URL)
         home_page = HomePage(self.driver)
         home_page.scroll_to_question(faq['index'])
         home_page.wait_question()
--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -1,68 +1,42 @@
-import time
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-
+from urls import Urls
 from pages.home_page import HomePage
 from pages.order_page import OrderPage
 from selenium.webdriver.support import expected_conditions as EC
+from data_for_tests import DataForTests as data
 
 
 class TestOrderPage:
     driver = None
-    BASE_URL = 'https://qa-scooter.praktikum-services.ru/'
-    samokats = [
-        {
-            'name': 'Истребитель',
-            'surname': 'Хорибл',
-            'address': 'Пушкина',
-            'metro': 'Чистые пруды',
-            'phone': '+79998877655',
-            'date': '03.09.2005',
-            'days_index': 1,
-            'color_index': 0,
-            'comment': 'сломался'
-        },
-        {
-            'name': 'Орешник',
-            'surname': 'Позитивный',
-            'address': 'Колотушкина',
-            'metro': 'Спортивная',
-            'phone': '+79998877777',
-            'date': '11.09.2023',
-            'days_index': 0,
-            'color_index': 1,
-            'comment': 'Работает'
-        }
-    ]
 
     @classmethod
     def setup_class(cls):
         cls.driver = webdriver.Firefox()
 
     def test_header_button_opens_order_form(self):
-        self.driver.get(self.BASE_URL)
+        self.driver.get(Urls.BASE_URL)
         order_page = OrderPage(self.driver)
         order_page.click_order_header()
         current_url = self.driver.current_url
-        expected_url = self.BASE_URL + 'order'
+        expected_url = Urls.ORDER_URL
         assert current_url == expected_url
         assert order_page.wait_order_form()
 
     def test_down_button_opens_order_form(self):
-        self.driver.get(self.BASE_URL)
+        self.driver.get(Urls.BASE_URL)
         order_page = OrderPage(self.driver)
         order_page.scroll_to_order_button_down()
         order_page.click_order_down()
         current_url = self.driver.current_url
-        expected_url = self.BASE_URL + 'order'
+        expected_url = Urls.ORDER_URL
         assert current_url == expected_url
         assert order_page.wait_order_form()
 
-    @pytest.mark.parametrize('samokats', samokats)
+    @pytest.mark.parametrize('samokats', data.samokats)
     def test_make_order_adds_new_samokat(self, samokats):
-        self.driver.get(self.BASE_URL + 'order')
+        self.driver.get(Urls.ORDER_URL)
         order_page = OrderPage(self.driver)
         order_page.fill_order_form(
             samokats['name'],
@@ -83,7 +57,7 @@ class TestOrderPage:
         home_page = HomePage(self.driver)
         order_page.click_button_watch_status()
         home_page.click_logo_samokat()
-        assert WebDriverWait(self.driver, 3).until(EC.url_to_be(self.BASE_URL))
+        assert WebDriverWait(self.driver, 3).until(EC.url_to_be(Urls.BASE_URL))
 
         home_page.click_logo_yandex()
         self.driver.switch_to.window(self.driver.window_handles[-1])
